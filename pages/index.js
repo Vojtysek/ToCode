081a5a3c01fef6b7bb72252b92c4f3dabb06420f
Vojtěch Döme <ojta@post.cz>
2022-08-02 12:43:34 +0200

    Listing && Checkbox

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,6 @@
 import Link from "next/link";
-import { useRef, useEffect } from "react";
-import "../styles/List.module.css";
 
 export default function Home({ codes }) {
-  let name = [];
-
-  codes.map(function (item) {
-    name.push(item.name);
-  });
-
-  useEffect(() => {
-    const firstUl = document.getElementById("firstUl");
-    const secondUl = document.getElementById("secondUl");
-
-    return () => {
-      for (let i = 0; i < name.length; i++) {
-       
-      }
-    };
-  }, [name]);
-
   return (
     <div className="grid place-items-center">
       <div className="flex py-10">
@@ -31,16 +12,17 @@ export default function Home({ codes }) {
         </Link>
       </div>
       <div className="flex w-full">
-        <ul id="firstUl" className="flex flex-col w-1/2">
-          <Link href="/">
-            <li className="flex cursor-pointer p-5 m-5 bg-sky-400 text-sm text-black rounded-lg sm:text-base lg:text-xl">
-              <p>{name[0]}</p>
-            </li>
-          </Link>
+        <ul id="firstUl" className="flex items-center w-full flex-col">
+          {codes.map((code) => (
+            <Link href={`/${code.name}`}>
+              <li className="flex cursor-pointer p-5 m-5 w-1/2 bg-sky-400 text-sm text-black rounded-lg sm:text-base lg:text-xl">
+                <a className="text-xxl font-bold">{code.name}</a>
+              </li>
+            </Link>
+          ))}
         </ul>
-        <ul id="secondUl" className="flex flex-col w-1/2"></ul>
       </div>
-      <button className="rounded-full w-12 h-12 text-2xl align-middle bg-sky-400">
+      <button className="rounded-full w-12 h-12 my-10 text-2xl align-middle bg-sky-400">
         <Link href="/add">
           <p>+</p>
         </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,5 +22,5 @@ module.exports = {
       // => @media (min-width: 1536px) { ... }
     }
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 }
